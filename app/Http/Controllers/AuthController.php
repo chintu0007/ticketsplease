032979080230198e3449111c15b7d3392117a64b
Gaurang Parekh <gaurang9rising@gmail.com>
2024-12-23 01:21:55 +0530

    working with postman and set login and register API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\Apiresponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use Apiresponses;
-    public function login() {
 
-        return $this->ok('Hello, Login with Traits!');
+     
+    public function login(ApiLoginRequest $request) {
 
-        // return response()->json([
-        //     'message' => 'Hello Login!'
-        // ], 200);
+        return $this->ok($request->get('email'));
+    
+    }
+    public function register() {
+        return $this->ok('Register');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login',[AuthController::class, 'login']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
+
 
 Route::get('/', function () {
     return response()->json([
