1a00e30a3947ad7a5e481a097a7d154e01a5c09b
Gaurang Parekh <gaurang9rising@gmail.com>
2024-12-27 14:45:01 +0530

    Authentication using tokens

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::apiResource('tickets',TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets',TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
